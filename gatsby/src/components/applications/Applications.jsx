86758a50d34403067c6d468e9b1fa3258c778a4a
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2020-10-22 19:43:41 -0700

    Saving progress wih applications

--- gatsby/src/components/applications/Applications.jsx
+++ gatsby/src/components/applications/Applications.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import SectionContentWrapper from '../shared/SectionContentWrapper';
+import SectionHeading from '../shared/SectionHeading';
+import ApplicationsCard from './ApplicationsCard';
+import ApplicationsGrid from './ApplicationsGrid';
+
 const ApplicationsStyles = styled.section`
   background: #ffffff;
 `;
 
 const Applications = ({ projects }) => {
-  console.log(projects);
+  const applications = projects.map((project) => {
+    return (
+      <ApplicationsCard
+        key={project.id}
+        application={project}
+      />
+    )
+  });
 
   return (
     <ApplicationsStyles id="applications" className="section">
-      <h1>APPLICATIONS SECTION</h1>
-      { projects.map((project) => project.projectName) }
+      <SectionContentWrapper>
+        <SectionHeading>
+          Applications
+        </SectionHeading>
+        <ApplicationsGrid>
+          { applications }
+        </ApplicationsGrid>
+      </SectionContentWrapper>
     </ApplicationsStyles>
   )
 }
