b760630e878025c66269032e2c122dbf92aabbff
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2020-11-14 13:19:25 -0800

    Formatted footer links better

--- gatsby/src/components/contact/Contact.jsx
+++ gatsby/src/components/contact/Contact.jsx
@@ -6,6 +6,8 @@ import {
 
 import Logo from '../shared/Logo';
 import SectionContentWrapper from '../shared/SectionContentWrapper';
+import ContactLinks from './ContactLinks';
+
 
 const ContactStyles = styled.section`
   background: var(--white);
@@ -14,24 +16,22 @@ const ContactStyles = styled.section`
   justify-content: center;
   align-items: center;
 
-  .logo-wrapper {
+  .contact-wrapper {
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
     grid-gap: 1.5rem;
-    align-items: center;
 
-    .divider {
-      height: 1px;
-      width: 100%;
-      background: var(--medium-grey);
+    .logo-wrapper {
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr;
+      grid-gap: 1.5rem;
+      align-items: center;
+
+      .divider {
+        height: 1px;
+        width: 100%;
+        background: var(--medium-grey);
+      }
     }
-  }
-
-  a {
-    display: grid;
-    grid-template-columns: auto auto;
-    width: max-content;
-    grid-gap: .5rem;
   }
 `;
 
@@ -45,17 +45,7 @@ const Contact = () => {
             <Logo />
             <div className="divider" />
           </div>
-          <div className="location-info">
-            <a
-              target="_blank"
-              className="animated-underline"
-              href="https://www.google.com/maps/place/Orange+County,+CA/@33.6410077,-118.0496294,10z/data=!3m1!4b1!4m5!3m4!1s0x80dc925c54d5f7cf:0xdea6c3618ff0d607!8m2!3d33.7174708!4d-117.8311428">
-              <LocationPin />
-              <span>
-                Orange County
-              </span>
-            </a>
-          </div>
+          <ContactLinks />
         </div>
       </SectionContentWrapper>
     </ContactStyles>
---
+++ gatsby/src/components/contact/ContactLinks.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import {
+  FiMapPin as LocationPin,
+  FiGithub as GitHub,
+  FiPhoneCall as Phone,
+  FiLinkedin as LinkedIn,
+  FiMail as Email,
+  FiFileText as Resume
+} from 'react-icons/fi';
+import styled from 'styled-components';
+
+const ContactLinksStyles = styled.div`
+  @media (min-width: 768px) {
+    display: grid;
+    grid-gap: 1.5rem;
+    grid-template-columns: 1fr 1fr 1fr;
+    justify-items: center;
+  }
+
+  @media (max-width: 767px) {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+  a {
+    display: grid;
+    grid-template-columns: auto auto;
+    width: max-content;
+    grid-gap: .5rem;
+    margin: 0rem 1rem;
+  }
+`;
+
+const ContactLinks = () => {
+
+  return (
+    <ContactLinksStyles>
+      <a
+        target="_blank"
+        className="animated-underline"
+        href="https://www.google.com/maps/place/Orange+County,+CA/@33.6410077,-118.0496294,10z/data=!3m1!4b1!4m5!3m4!1s0x80dc925c54d5f7cf:0xdea6c3618ff0d607!8m2!3d33.7174708!4d-117.8311428">
+        <LocationPin />
+        <span>
+          Orange County
+        </span>
+      </a>
+      <a
+        target="_blank"
+        className="animated-underline"
+        href="https://www.google.com/maps/place/Orange+County,+CA/@33.6410077,-118.0496294,10z/data=!3m1!4b1!4m5!3m4!1s0x80dc925c54d5f7cf:0xdea6c3618ff0d607!8m2!3d33.7174708!4d-117.8311428">
+        <Phone />
+        <span>
+          949-202-7141
+        </span>
+      </a>
+      <a
+        target="_blank"
+        className="animated-underline"
+        href="https://www.google.com/maps/place/Orange+County,+CA/@33.6410077,-118.0496294,10z/data=!3m1!4b1!4m5!3m4!1s0x80dc925c54d5f7cf:0xdea6c3618ff0d607!8m2!3d33.7174708!4d-117.8311428">
+        <Email />
+        <span>
+          kevin.akahoshi@gmail.com
+        </span>
+      </a>
+    </ContactLinksStyles>
+  )
+}
+
+export default ContactLinks;
