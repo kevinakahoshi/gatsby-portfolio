2af511f8ca592e66880eec983023e7289442ac1a
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2021-01-23 17:52:07 -0800

    Cleaned up some of the JSX files

--- gatsby/src/components/about/AboutBioParagraphs.jsx
+++ gatsby/src/components/about/AboutBioParagraphs.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 
-const AboutBioParagraphs = ({ bio }) => {
-  return bio.map((item) => {
-    return (
-      <p key={item.children[0]._key}>
-        { item.children[0].text}
-      </p>
-    )
-  });
-}
+const AboutBioParagraphs = ({ bio }) => bio.map((item) => (
+    <p key={item.children[0]._key}>
+      { item.children[0].text}
+    </p>
+  ));
 
 export default AboutBioParagraphs;
