9a32ebb1fc863fb4da0a8a0b1c488e2a6fcfaacb
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2021-02-20 18:10:53 -0800

    Removed spinner and updated keyframes

--- gatsby/src/components/hero/HeroImage.jsx
+++ gatsby/src/components/hero/HeroImage.jsx
@@ -1,49 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
-import HeroImageBackground from './HeroImageBackground';
-import HeroImageBelowTheFold from './HeroImageBelowTheFold';
-import HeroImageElementsLayout from './HeroImageElementsLayout';
-import HeroImageHeader from './HeroImageHeader';
-import HeroImageHero from './HeroImageHero';
-import HeroImageShadowDOM from './HeroImageShadowDOM';
+import HeroImageContents from './HeroImageContents';
 
 const HeroImageStyles = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   position: relative;
-  transform: perspective(1250px) rotateY(-30deg) rotateX(15deg);
-  display: grid;
+  transform: translate3d(0,0,0);
 
-  *:not(.transition-backround) {
-    border-radius: .25rem;
-  }
-
-  @media (min-width: 768px) {
-    height: clamp(350px, 50vmin, 50vmin);
-    width: clamp(350px, 50vmin, 50vmin);
-  }
-
-  @media (min-width: 381px) and (max-width: 767px) {
-    height: 45vmax;
-    width: 45vmax;
-  }
-
-  @media (max-width: 380px) {
-    width: 95%;
-    padding-top: 95%;
+  @media (max-width: 767px) {
+    grid-row: 1;
   }
 `;
 
-const HeroImage = () => {
-  return (
-    <HeroImageStyles>
-      <HeroImageElementsLayout>
-        <HeroImageHeader />
-        <HeroImageHero />
-        <HeroImageBelowTheFold />
-      </HeroImageElementsLayout>
-      <HeroImageBackground />
-      <HeroImageShadowDOM />
-    </HeroImageStyles>
-  )
-};
+const HeroImage = ({ children }) => (
+  <HeroImageStyles>
+    <HeroImageContents />
+  </HeroImageStyles>
+);
 
 export default HeroImage;
