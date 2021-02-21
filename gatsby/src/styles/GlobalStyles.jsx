9276f35d7674b9ebe798c1394c7d7999dabda6cb
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2021-02-20 18:42:55 -0800

    Removed unused keyframes

--- gatsby/src/styles/GlobalStyles.jsx
+++ gatsby/src/styles/GlobalStyles.jsx
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 import './fonts.css';
+import { SlideIn, SlideOut } from './Keyframes';
 
 const GlobalStyles = createGlobalStyle`
   :root {
@@ -32,18 +33,13 @@ const GlobalStyles = createGlobalStyle`
     padding: 5rem 1rem;
   }
 
-  .fade-up {
-    animation: fade-up 1s forwards;
-    animation-fill-mode: both;
-  }
-
   .slide-in {
-    animation: slide-in .75s forwards;
+    animation: ${SlideIn} .75s forwards;
     animation-fill-mode: both;
   }
 
   .slide-out {
-    animation: slide-out .75s forwards;
+    animation: ${SlideOut} .75s forwards;
     animation-fill-mode: both;
   }
 
@@ -53,95 +49,6 @@ const GlobalStyles = createGlobalStyle`
 
   .no-js {
     opacity: 0;
-  }
-
-  @keyframes fade-up {
-    0% {
-      opacity: 0;
-      transform: translateY(10%);
-    }
-
-    100% {
-      opacity: 1;
-      transform: translateY(0%);
-    }
-  }
-
-  @keyframes skelly-swipe {
-    0% {
-      background-position: 100%;
-    }
-
-    100% {
-      background-position: -100%;
-    }
-  }
-
-  @keyframes pulse {
-    0% {
-      opacity: .5;
-    }
-
-    50% {
-      opacity: 1;
-    }
-
-    100% {
-      opacity: .5;
-    }
-  }
-
-  @keyframes slide-in {
-    0% {
-      opacity: 0;
-      transform: translateY(25%);
-    }
-
-    100% {
-      opacity: 1;
-      transform: translateY(0%);
-    }
-  }
-
-  @keyframes slide-out {
-    0% {
-      opacity: 1;
-      transform: translateY(0%);
-    }
-
-    100% {
-      opacity: 0;
-      transform: translateY(25%);
-    }
-  }
-
-
-  @keyframes float {
-    0% {
-      transform: translateY(0%) translateX(0%);
-    }
-
-    50% {
-      transform: translateY(-1.5%) translateX(-0.5%);
-    }
-
-    100% {
-      transform: translateY(0%) translateX(0%);
-    }
-  }
-
-  @keyframes element-float {
-    0% {
-      transform: translateY(0%) translateX(0%);
-    }
-
-    50% {
-      transform: translateY(-7.5%) translateX(-2.5%);
-    }
-
-    100% {
-      transform: translateY(0%) translateX(0%);
-    }
   }
 `;
 
