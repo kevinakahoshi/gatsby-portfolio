ce707659023ac1513fc573a9a5626afb1a0533e3
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2021-02-24 22:13:46 -0800

    Removed TODOs

--- gatsby/src/components/hero/HeroImageBelowTheFold.jsx
+++ gatsby/src/components/hero/HeroImageBelowTheFold.jsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import { Float } from '../../styles/Keyframes';
 import HeroImageTextBlock from './HeroImageTextBlock';
 
-// TODO: Add clamp to grid-gap
 const HeroImageBelowTheFoldStyles = styled.div`
   display: grid;
   grid-template-columns: 1fr 3fr;
