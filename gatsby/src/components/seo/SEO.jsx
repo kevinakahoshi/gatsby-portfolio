be0f5fd240dc91bfbc5e64c5f95550f105e47a62
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2021-03-02 18:04:56 -0800

    Updated applications buttons for safari

--- gatsby/src/components/seo/SEO.jsx
+++ gatsby/src/components/seo/SEO.jsx
@@ -57,6 +57,8 @@ const SEO = ({ location, children }) => {
         { siteTitle }
       </title>
       <link rel='icon' href={siteFavicon.asset.url} type='image/svg+xml' />
+      <link rel='icon' href="/favicon.svg" type='image/svg+xml' />
+      <link rel='apple-touch-icon' href={siteFavicon.asset.url} type='image/svg+xml' />
       <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       <meta charset='UTF-8' />
       <meta name='description' content={siteDescription || description} />
